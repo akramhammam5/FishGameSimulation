5039388451e3592354c5d9ce7f28f9b2a510246d
Akram Hammam <63311262+akramhammam5@users.noreply.github.com>
2022-12-04 23:52:04 +0200

    Finsishing up phase 1

--- JS/threee.js
+++ JS/threee.js
@@ -1,55 +1,62 @@
 //importing THREE to a namespace called 'three'.
 import * as THREE from 'three';
-import { AxesHelper, BoxGeometry, Light, PerspectiveCamera } from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { AxesHelper, BoxGeometry, Light, PerspectiveCamera } from 'three'; //kol elhagat elmabein el braces dol bn3mlhom import mn threejs library
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';//orbitcontrol ashan n3rf nstkhdm elmouse input
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';//ashan ykhliny a3rf anzl el gltf wnzlha ka objects
 
-import sea from './underwater-world-ocean-corals-light-11570012516i3q0gtwkal.jpeg';
+import sea from './underwater-world-ocean-corals-light-11570012516i3q0gtwkal.jpeg'; 
 
-const fish = new URL('./shiny_fish.glb',import.meta.url);
+const fish = new URL('./shiny_fish.glb',import.meta.url); //constant variables bn3mlha create ashan n2dr nnady 3aliha taht fel code
 
-const sea2 = new URL('./animated_ocean_scene_tutorial_example_1.glb',import.meta.url);
+//const sea2 = new URL('./animated_ocean_scene_tutorial_example_1.glb',import.meta.url);
 
 const octopus = new URL('./happy_octopus.glb',import.meta.url);
 
+const goldfish = new URL('./Goldfish.glb',import.meta.url);
+
+const starfish = new URL('./Starfish.glb',import.meta.url);
+
+const kingfish = new URL('./Kingfish.glb',import.meta.url);
+
+
+
 
 //Add and animate all the stuff in a web page space using webgl.
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer(); //ben-create render ashan kol mab3ml function baktb b3dha render ashan yrsm el object
 
 //All over the page.
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth, window.innerHeight ); // de bta3t window kolha
 
-document.body.appendChild(renderer.domElement);
+document.body.appendChild(renderer.domElement);// da byshghl elcanvas nfso 
 
 
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 500 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 500 );//75 da el angle 1 wel 500 dol near wel far w elwindow.innerWidth da el aspectRatio
 
-//orbit = new OrbitControls(camera , renderer.domElement);
+orbit = new OrbitControls(camera , renderer.domElement);
 
-camera.position.set( 0, 0, 30 );
+camera.position.set( 0, 0, 30 );//.set btb2a function gahza bgbly el x,y,z 
 
 //Call upadate method every time camera position is changed.
-//orbit.update();
+orbit.update();
 
-camera.lookAt( 0, 0, 0 );
+//camera.lookAt( 0, 0, 0 );
 
 //Creating A scene.
 
-const scene1 = new THREE.Scene();
+const scene1 = new THREE.Scene();//bnhot gwa el objects
 
 //Defining components of a Box.
 textureloader = new THREE.TextureLoader();
 scene1.background = textureloader.load(sea);
 
-const loader = new GLTFLoader();
+const loader = new GLTFLoader();//da ashan nhot el objects nfsha
 
-loader.load(fish.href,function(gltf){
+loader.load(fish.href,function(gltf){//load de function gahza btakhod aktr mn perameter el fish.href ashan ageib refrence bta3 object whoto wfunction(gltf) bnstkhdmo ashan nhot el object gwa elscene w nzboto
     model = gltf.scene;
-    scene1.add(model);
-    model.scale.set(1,1,1);
+    scene1.add(model);//bthotly el object fel scene
     model.position.set(0,0,0);
-},undefined,function(error){
+},function(error){
     console.log("error");
 })
 
@@ -57,14 +64,46 @@ loader.load(fish.href,function(gltf){
 loader.load(octopus.href,function(gltf){
     model1 = gltf.scene;
     scene1.add(model1);
-    
-    model1.position.set(-20,-5,-30 )
-},undefined,function(error){
+    model1.position.set(-10,-10,-30 );
+    mixer= new THREE.AnimationMixer(gltf.scene);
+    animationAciton=mixer.clipAction(gltf.animations[0]).play();
+    animationActions.push(animationAction);
+
+},function(error){
     console.log("error");
 })
 
+
+loader.load(goldfish.href,function(gltf){
+    model2 = gltf.scene;
+    scene1.add(model2);
+    
+    model2.position.set(-100,50,-200)
+},function(error){
+    console.log("error");
+})
+
+loader.load(starfish.href,function(gltf){
+    model3 = gltf.scene;
+    scene1.add(model3);
+    
+    model3.position.set(-30,-10,-20)
+},function(error){
+    console.log("error");
+})
+
+loader.load(kingfish.href,function(gltf){
+    model4 = gltf.scene;
+    scene1.add(model4);
+    
+    model4.position.set(80,60,-150)
+},function(error){
+    console.log("error");
+})
+
+
 	 
-const ambientLight = new THREE.AmbientLight(0x333333);
+const ambientLight = new THREE.AmbientLight(0x333333); //create ambientLight
 scene1.add(ambientLight);
 
 const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
@@ -104,24 +143,24 @@ var x=1;
         
       };
 
-function animate() {
     
-    requestAnimationFrame( animate );
-     z+=0.0001;
-     if(z>=0){
-         z=-30;
-     }
 
-     model1.translateZ(z);
-    
-    renderer.render( scene1, camera );
-}
-var rotateOctpus = function(){
-    requestAnimationFrame(rotateOctpus);
-    model1.translateZ(-1);
-    model1.rotateX(0.05);
-    model1.translateZ(+1);
+var rotateGoldfish = function(){
+    requestAnimationFrame(rotateGoldfish);
+    model2.translateZ(-2);
+    model2.rotateY(0.05);
+    model2.rotateX(0.02);
+    model2.translateZ(+0.09);
     renderer.render(scene1, camera);
-    };
-rotateOctpus();
-animate();
+};
+
+/*var rotateStarfish = function(){
+    requestAnimationFrame(rotateStarfish);
+    model3.translateZ(-2);
+    model3.rotateY(0.5);
+    model3.translateZ(+2);
+    renderer.render(scene1, camera);
+
+}; */
+rotateGoldfish();
+//rotateStarfish();
